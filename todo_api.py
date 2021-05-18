3a62dfda18fc6a2ba160e0dee69f861fed13f8e2
SrivarshaElango <84168061+SrivarshaElango@users.noreply.github.com>
2021-05-18 15:26:01 +0530

    Update todo_api.py

--- todo_api.py
+++ todo_api.py
@@ -12,7 +12,9 @@ conn = cx_Oracle.connect(user=r'SYSTEM', password='O4oracle', dsn=dsn_tns)
 
 c=conn.cursor()
 c.execute('drop table Tasks')
-c.execute('create table Tasks (id number(2), task varchar2(20), due_by date, status varchar2(20))')
+c.execute('create table Tasks (id number(2), task varchar2(20), due_by varchar2(10), status varchar2(20))')
+c.execute('commit')
+conn.close()
 
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
@@ -30,6 +32,18 @@ todo = api.model('Todo', {
 })
 
 stat_val = ['Not Started', 'In Progress', 'Finished']
+
+#Function prints database table contents everytime all tasks are listed using GET in API
+def disp_dbtab():
+    print('Table contents : ')
+    print('Id\t Task\t\t Due_Date\t Status')
+    conn = cx_Oracle.connect(user=r'SYSTEM', password='O4oracle', dsn=dsn_tns)
+    c=conn.cursor()
+    c.execute('select * from Tasks')
+    for row in c:
+    	print(row[0], '\t', row[1], ' \t', row[2], '\t', row[3])
+    conn.close()
+
 
 class TodoDAO(object):
     def __init__(self):
@@ -85,8 +99,11 @@ class TodoDAO(object):
         self.todos.append(todo)
         d=list(todo.values())
         d=[d[-1]]+d[:-1]
-        #print(d)
+        conn = cx_Oracle.connect(user=r'SYSTEM', password='O4oracle', dsn=dsn_tns)
+        c=conn.cursor()
         c.execute("insert into Tasks values (:id,:task,to_date(:due_by, 'YYYY-MM-DD'),:status)",d)
+        c.execute('commit')
+        conn.close()
         return todo
 
     def update(self, id, data):
@@ -94,17 +111,32 @@ class TodoDAO(object):
             return
         todo = self.get(id)
         todo.update(data)
+        d=list(data.values())
+        d+=[id]
+        conn = cx_Oracle.connect(user=r'SYSTEM', password='O4oracle', dsn=dsn_tns)
+        c=conn.cursor()
+        c.execute("update Tasks set status=:status where id=:id",d)
+        c.execute('commit')
+        conn.close()
         return todo
 
     def delete(self, id):
         todo = self.get(id)
         self.todos.remove(todo)
+        d=[]
+        d+=[id]
+        conn = cx_Oracle.connect(user=r'SYSTEM', password='O4oracle', dsn=dsn_tns)
+        c=conn.cursor()
+        c.execute("delete from Tasks where id=:id",d)
+        c.execute('commit')
+        conn.close()
 
 
 DAO = TodoDAO()
 DAO.create({'task': 'Build an API', 'due by': '2021-02-15', 'status': 'Not Started'})
 DAO.create({'task': '?????', 'due by': '2021-10-10', 'status': 'Finished'})
 DAO.create({'task': 'profit!', 'due by': '2021-08-20', 'status': 'In Progress'})
+
 
 @ns.route('/')
 class TodoList(Resource):
@@ -113,6 +145,7 @@ class TodoList(Resource):
     @ns.marshal_list_with(todo)
     def get(self):
         '''List all tasks'''
+        disp_dbtab()
         return DAO.todos
 
     @ns.doc('create_todo')
@@ -121,6 +154,7 @@ class TodoList(Resource):
     def post(self):
         '''Create a new task'''
         return DAO.create(api.payload), 201
+
 
 @ns.route('/<string:due_date>')
 @ns.response(404, 'Todo not found')
@@ -133,6 +167,7 @@ class TodoDue(Resource):
         '''List all tasks due on the given date'''
         return DAO.get_due(due_date)
 
+
 @ns.route('/overdue')
 class TodoOverdue(Resource):
     '''Lists overdue tasks'''
@@ -142,6 +177,7 @@ class TodoOverdue(Resource):
         '''List all overdue tasks'''
         return DAO.get_overdue()
 
+
 @ns.route('/finished')
 class TodoFinished(Resource):
     '''Lists completed/finished tasks'''
@@ -150,6 +186,7 @@ class TodoFinished(Resource):
     def get(self):
         '''List all finished tasks'''
         return DAO.get_finished()
+
 
 @ns.route('/<int:id>')
 @ns.response(404, 'Todo not found')
@@ -169,18 +206,17 @@ class Todo(Resource):
         DAO.delete(id)
         return '', 204
 
+
+@ns.route('/status/<int:id>')
+@ns.response(404, 'Todo not found')
+@ns.param('id', 'The task identifier')
+class TodoUpdate(Resource):
     @ns.expect(todo)
     @ns.marshal_with(todo)
     def put(self, id):
-        '''Update a task given its identifier'''
+        '''Update status of a task given its identifier'''
         return DAO.update(id, api.payload)
 
-print('Table contents : ')
-print('Id\t Task\t\t Due_Date\t Status')
-c.execute('select * from Tasks')
-for row in c:
-	print(row[0], '\t', row[1], ' \t', row[2], '\t', row[3])
-conn.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
